e1b59c159e74abc35d05558820efcd927f85c6d6
student <alisherdaminov1912199427@gmail.com>
2022-05-22 10:05:23 +0500

    pl

---
+++ app/src/main/java/com/example/a662java/G_1.java
@@ -0,0 +1,9 @@
+package com.example.a662java;
+
+public class G_1 {
+    int image;
+
+    public G_1(int image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/example/a662java/G_2.java
@@ -0,0 +1,9 @@
+package com.example.a662java;
+
+public class G_2 {
+    int image;
+
+    public G_2(int image) {
+        this.image = image;
+    }
+}
